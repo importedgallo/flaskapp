c92e18162b2690d2b4bb744dd126f39f01841e27
importedGallo <andre.mongard@gmail.com>
2021-09-20 13:07:56 +1000

    added support for query parameters /profile?name=

--- views.py
+++ views.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html", name="Andre", age=21)
+    return render_template("index.html", name="andre", age=21)
 
 
-@views.route("/profile/<username>")
-def profile(username):
-    return render_template("index.html", name=username)
+@views.route("/profile")
+def profile():
+    args = request.args
+    name = args.get('name')
+    return render_template("index.html", name=name)
