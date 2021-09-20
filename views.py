f87c35612b2141e331a99877ac8fa4d8f50e429f
importedGallo <andre.mongard@gmail.com>
2021-09-20 13:14:37 +1000

    added support for returning json data

--- views.py
+++ views.py
@@ -1,14 +1,21 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 
 views = Blueprint(__name__, "views")
 
+# home page
 @views.route("/")
 def home():
     return render_template("index.html", name="andre", age=21)
 
-
+# query parameters
 @views.route("/profile")
 def profile():
     args = request.args
     name = args.get('name')
     return render_template("index.html", name=name)
+
+# return json data
+@views.route("/json")
+def get_json():
+    return jsonify({'name': 'andre', 'coolness': 10})
+
