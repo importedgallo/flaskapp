26df3fb530e3de8ee9d24243fee566f45ddff8c2
importedGallo <andre.mongard@gmail.com>
2021-09-20 12:59:47 +1000

    added profile() route in views.py

--- views.py
+++ views.py
@@ -4,4 +4,9 @@ views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", name="Andre", age=21)
+
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name=username)
