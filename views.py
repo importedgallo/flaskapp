dec21d175a35119647702c05a658ee3849bdfc0f
importedGallo <andre.mongard@gmail.com>
2021-09-20 13:19:35 +1000

    added support for redirecting urls

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
 
 views = Blueprint(__name__, "views")
 
@@ -19,3 +19,14 @@ def profile():
 def get_json():
     return jsonify({'name': 'andre', 'coolness': 10})
 
+# get json data
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+#redirect to a different page
+@views.route("/go-to-home")
+def got_to_home():
+    return redirect(url_for("views.home"))
+    
