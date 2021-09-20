ee8d0abf9fbf65c51da2b9222036aa799c96d936
importedGallo <andre.mongard@gmail.com>
2021-09-20 12:35:45 +1000

    moved home() to views.py while importing views.py into app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
+from views import views
 
 app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/")
 
-@app.route("/")
-def home():
-       return "this is the home page"
 
 if __name__ == '__main__':
         app.run(debug=True, port=8000)
